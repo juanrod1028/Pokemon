7182b3f53dd3ce54c739e3212cfe21d93408c527
Juan Camilo Rodriguez Giraldo <juanrodriguez1028@gmail.com>
2022-11-09 13:25:04 -0500

    segundo commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 export const searchPokemon = async (pokemon) => {
   try {
     // let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-    let url = `http://localhost:8080/pokemon/${pokemon}`;
+    // let url = `http://localhost:8080/pokemon/${pokemon}`;
+    let url = `https://api-dev.terpel.com/pokemon/${pokemon}`;
     const response = await fetch(url);
     const data = await response.json();
     return data;
@@ -11,7 +12,7 @@ export const searchPokemon = async (pokemon) => {
 export const getPokemons = async (limit = 12, offset = 0) => {
   try {
     // let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-    let url = `http://localhost:8080/pokemon?limit=${limit}&offset=${offset}`;
+    let url = `https://api-dev.terpel.com/pokemon?limit=${limit}&offset=${offset}`;
     const response = await fetch(url);
     const data = await response.json();
     return data;
