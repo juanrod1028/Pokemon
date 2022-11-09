a670a788879f2659d5151081efc871f32916c4aa
Juan Camilo Rodriguez Giraldo <juanrodriguez1028@gmail.com>
2022-11-09 12:01:03 -0500

    Updates

---
+++ precache-manifest.975bda43a6a9182527c83e97ee24f0d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78b7611000f87afb77d7",
+    "url": "/Pokemon/static/css/main.9cf236cc.chunk.css"
+  },
+  {
+    "revision": "78b7611000f87afb77d7",
+    "url": "/Pokemon/static/js/main.78b76110.chunk.js"
+  },
+  {
+    "revision": "6acab62222c9ba68f6c9",
+    "url": "/Pokemon/static/js/1.6acab622.chunk.js"
+  },
+  {
+    "revision": "d15fcab501317f2983dd",
+    "url": "/Pokemon/static/js/runtime~main.d15fcab5.js"
+  },
+  {
+    "revision": "166b639e9110d43c600d5df599f3d821",
+    "url": "/Pokemon/index.html"
+  }
+];
